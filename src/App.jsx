306d504152037a4ee2e453fed76eaeb9c3157881
Crystal Soto <crystalsotogd@yahoo.com>
2024-01-15 22:18:32 -0600

    imported and created function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import ContactList from './components/ContactList';
+import SelectedContact from "./components/SelectedContact";
 
-function App() {
-
-  return (
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+  if (!selectedContactId){
+    return (
     <>
-      
+    <ContactList key="ContactList" setSelectedContactId={setSelectedContactId}/>
     </>
-  )
+    );
+  } else {
+    return (
+      <>
+      <SelectedContact key={selectedContactId} selectedContactId={selectedContactId} setSelectedContactId={setSelectedContactId}/>
+      </>
+    )
+  }
 }
-
-export default App
